7b12a13ce6314dedf6b8c6ea7fc0666ea37963d1
jasser abdelfattah <120596258+jassercmk1@users.noreply.github.com>
2023-04-10 22:08:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,32 @@
+# The starting code with sys and ends with True.
+# is for preventing the generation of bytecode files.
 import sys
 sys.dont_write_bytecode = True
-from freightcar import FreightCar
-from station import Station
 from container import Container, NormalContainer, HeavyContainer, RefrigeratedContainer, LiquidContainer
-
+from station import Station
+from freightcar import FreightCar
 
 
 class Main():
+    # Test Case 1
+    print("|=============== Test Case 1 ===============|")
     c1 = Container(1, 500, 'NormalContainer', 0)
     c2 = Container(2, 3500, 'RefrigeratedContainer', 1)
     print(c1)
     print(c2)
-
+    # Test Case 2
+    print("|=============== Test Case 2 ===============|")
     station = Station(0, 'Station_0', [])
     fc = FreightCar(1, station, 10000, 10, 5, 3, 4, 50.0)
     print(str(fc))
+    # Test Case 3
+    print("|=============== Test Case 3 ===============|")
     print(Station.create_station(3, 40.0, 50.0))
+    # Test Case 4
+    print("|=============== Test Case 4 ===============|")
     Freight_Car = FreightCar(0, Station(
         2, 'Station_2', []), 14000, 10, 5, 4, 0, 0.0)
     print(str(Freight_Car))
-
     station = Station.create_station(0, 0, 0)
     freight_car = FreightCar(0, station, 14000, 10, 5, 4, 0.3, 0.0)
 
@@ -34,9 +41,10 @@ class Main():
     current_containers = freight_car.getCurrentContainers()
     for container in current_containers:
         print(container)
-
     total_weight = sum(container.weight for container in current_containers)
     print("Total weight of containers in freight car:", total_weight)
+    # Test Case 5
+    print("|================ Test Case 5 ===============|")
 
     station3 = Station.create_station(3, 0.0, 0.0)
     freight_car0 = FreightCar(0, station3, 10000, 10, 5, 3, 2, 2.0)
@@ -47,6 +55,8 @@ class Main():
 
     freight_car0.unload_container(1, station3)
     print(freight_car0)
+    # Test Case 6
+    print("|=============== Test Case 6 ===============|")
 
     freight_car = FreightCar(ID=0, currentStation=Station(ID=0, X=0, Y=0), totalWeightCapacity=10000,
                              maxNumberOfAllContainers=10, maxNumberOfHeavyContainers=5,
@@ -55,15 +65,18 @@ class Main():
     destination_station = Station(ID=1, X=10, Y=10)
     has_enough_fuel = freight_car.calculate_fuel_consumption(
         destination_station)
-    print("Fuel Consumption : {0}".format(has_enough_fuel))
+    print("[ Fuel Consumption : {0} ]".format(has_enough_fuel))
+    # Test Case 7
+    print("|=============== Test Case 7 ===============|")
 
     freight_car = FreightCar(ID=0, currentStation=Station(ID=0, X=0, Y=0), totalWeightCapacity=10000,
                              maxNumberOfAllContainers=10, maxNumberOfHeavyContainers=5,
                              maxNumberOfRefrigeratedContainers=2, maxNumberOfLiquidContainers=2,
                              double_fuelConsumptionPerKM=0.1)
     freight_car.add_fuel(30.20)
-    print("Fuel Added : {0}".format(freight_car.double_fuel))
+    print("[ Fuel Added : {0} ] ".format(freight_car.double_fuel))
 
 
+# Driver Code
 if __name__ == '__main__':
     main = Main()
