84366125d76199f4070e6799d03c0b519620d556
jasser abdelfattah <120596258+jassercmk1@users.noreply.github.com>
2023-04-13 15:00:43 +0200

    Update station.py

--- station.py
+++ station.py
@@ -10,9 +10,17 @@ class Station:
     def __str__(self) -> str:
         return f"Station :\n[ ID : {self.ID}\n X : {self.X}\n Y : {self.Y} ]"
 
+    @staticmethod
     def create_station(station_id: int, x: float, y: float):
         station = Station(station_id, x, y)
         return station
 
     def getDistance(self, other) -> float:
         return ((self.X - other.X) ** 2 + (self.Y - other.Y) ** 2) ** 0.5
+
+    def incoming_freightcar(self, freightcar):
+        self.current.append(freightcar)
+
+    def outgoing_freightcar(self, freightcar):
+        self.history.append(freightcar)
+        self.current.remove(freightcar)
